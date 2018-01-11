cf598229f11568a4aa8f02f7bc73db15be3469be
StefanO <StefanO@ppedv.de>
2018-01-11 12:29:49 +0100

    Beta - Commit
    Diverese Fehlerbehebungen
    Es können nun mehrere RemoteComputer übergeben werden
    Angepasste ausgabe

--- Get-LocalAdmin.ps1
+++ Get-LocalAdmin.ps1
@@ -2,7 +2,11 @@
 .Synopsis
    Anzeigen der Lokalen Administratoren
 .DESCRIPTION
-   cmdLet zum auslesen der lokalen User auf lokalem oder Remote Computer welche zu den lokalen Administratoren gehören
+   Skript zum auslesen der User auf einem oder mehreren Remote Computern welche zu den lokalen Administratoren gehören
+.Notes
+    Version: 1.0
+    Author:  Stefan Ober
+    E-Mail:  StefanO (at) ppedv.de
 .EXAMPLE
    .\Get-LocalAdmin.ps1 -ComputerName localhost
 #>
@@ -10,22 +14,41 @@
 [OutputType([string[]])]
 Param(
     [Parameter(Mandatory = $true)]
-    [ValidateScript({Test-Connection -Quiet -ComputerName $PSItem})]
-    [string]$ComputerName
+    [ValidateScript({Write-Host "Prüfen der Verbindung" -ForeGroundColor Yellow;
+                     if(Test-Connection -Quiet -ComputerName $PSItem )
+                     {
+                        Write-Host "Prüfung erfolgreich, $PSitem erreichbar über ICMP" -ForegroundColor Green
+                        $true
+                     }else
+                     {
+                        throw "Gerät nicht erreichbar"
+                     }})]
+    [string[]]$ComputerName
 )
-    Clear-Variable UserNames
-
-    $group = Get-WmiObject win32_group -Filter "LocalAccount=True AND SID='S-1-5-32-544'"
-    $query = "GroupComponent = `"Win32_Group.Domain='$($group.domain)'`,Name='$($group.name)'`""
-    $list = Get-WmiObject -Query "Select * FROm Win32_GroupUser WHERE $Query" -ComputerName "$Computername"
-
-    Write-Verbose ($list.PartComponent).ToString()
-
-    foreach($Name in $list)
+    $Ergebnisse = New-Object System.Collections.Generic.List[object]
+    foreach($Computer in $ComputerName)
     {
-        $UserNames = $UserNames,$(($Name).PartComponent.split(','))[1]
-    }
-
-    return $UserNames
     
+        if($UserNames -ne $null)
+        {
+            Clear-Variable UserNames
+        }
+        $group = Get-WmiObject win32_group -Filter "LocalAccount=True AND SID='S-1-5-32-544'" -Computer $Computer
+        $query = "GroupComponent = `"Win32_Group.Domain='$($group.domain)'`,Name='$($group.name)'`""
+        $list = Get-WmiObject -Query "Select * FROM Win32_GroupUser WHERE $Query" -Computer "$Computer"
 
+        Write-Verbose ($list.PartComponent).ToString()
+    
+            foreach($Name in $list)
+            {        
+                $Ergebniss = New-Object Psobject -Property @{
+                                                            Computer= $Name.PSComputerName
+                                                            UserName = $($($(($Name).PartComponent.split(','))[1]).TrimStart('Name="')).TrimEnd('"')
+                                                        }
+                $Ergebnisse.Add($Ergebniss)
+            }
+    }
+    return $Ergebnisse
+   
+
+    
